8df9cf2934127543a0aca85aff688761f7f93ead
Studentofcodeinstitute <>
2024-01-31 11:17:34 +0000

    Add new three test file

---
+++ todo/test_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class TestDjango(TestCase):
+
+    def test_this_thing_work1(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_work2(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_work3(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_work4(self):
+        self.assertEqual(1, 1)
